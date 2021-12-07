1408326f2c1ca14477eef8e6724add31b04a0fcf
Joe Gasewicz <josefgasewicz@gmail.com>
2021-12-07 22:36:18 +0000

    Handle bad access: nil dereference Fixes #2

--- fileManager.go
+++ fileManager.go
@@ -23,31 +23,31 @@ func (f *FileManager) setEntityDirPath(id uint) (string, error) {
 	return fullDirPath, err
 }
 
-// Gets the full entity file path only (does not include the filename)
+// GetEntityDirPath Gets the full entity file path only (does not include the filename)
 func (f *FileManager) GetEntityDirPath(id uint) string {
 	fullDirPath := fmt.Sprintf("%s/%s/%d", f.fullDirPath, f.TableName, id)
 	return fullDirPath
 }
 
-// GetEntityUrl gets the full url
+// GetEntityURL GetEntityUrl gets the full url
 func (f *FileManager) GetEntityURL(fileName string, id uint) string {
 	url := fmt.Sprintf("%s/%s/%s/%d/%s", f.URL, f.UploadDir, f.TableName, id, fileName)
 	return url
 }
 
-// Get the full path which is constructed of the following:
+// GetEntityFilePath Get the full path which is constructed of the following:
 // upload path + entity name + plus the primary id + the filename
 // e.g. `./assets/uploads/users/1/cats.jpg`
 func (f *FileManager) GetEntityFilePath(fullEntityDirPath string, fileName string) string {
 	return fmt.Sprintf("%s/%s", fullEntityDirPath, fileName)
 }
 
-// Uploads & stores the file on your server. If there is an error then the return value of string
+// Upload Uploads & stores the file on your server. If there is an error then the return value of string
 // will be an empty string. If there are no errors to return the string will be the full path of
 // with filename.
 func (f *FileManager) Upload(w http.ResponseWriter, r *http.Request, id uint, formName string) (string, error) {
 	var err error
-	r.ParseMultipartForm(50 << 20)
+	r.ParseMultipartForm(50 << 20) // 50mb
 	file, handler, err := r.FormFile(formName)
 	defer file.Close()
 	if err != nil {
@@ -73,13 +73,13 @@ func (f *FileManager) Upload(w http.ResponseWriter, r *http.Request, id uint, fo
 	return fullEntityFilePath, err
 }
 
-// Gets the full file path including the filename
+// Get Gets the full file path including the filename
 func (f *FileManager) Get(fileName string, id uint) string {
 	entityFilePath := f.GetEntityURL(fileName, id)
 	return entityFilePath
 }
 
-// Updates only the filename not the file (Use Upload to update the file)
+// Update Updates only the filename not the file (Use Upload to update the file)
 func (f *FileManager) Update(fileName string, id uint, newFileName string) error {
 	var err error
 	entityDirPath := f.GetEntityDirPath(id)
@@ -89,7 +89,7 @@ func (f *FileManager) Update(fileName string, id uint, newFileName string) error
 	return err
 }
 
-// Deletes the file from the entity file path
+// Delete Deletes the file from the entity file path
 func (f *FileManager) Delete(fileName string, id uint) error {
 	entityDirPath := f.GetEntityDirPath(id)
 	entityFilePath := f.GetEntityFilePath(entityDirPath, fileName)
@@ -97,7 +97,7 @@ func (f *FileManager) Delete(fileName string, id uint) error {
 	return err
 }
 
-// Deletes a single entity by a UUID
+// DeleteEntityByID Deletes a single entity by a UUID
 func (f *FileManager) DeleteEntityByID(id uint) error {
 	entityDirPath := f.GetEntityDirPath(id)
 	err := os.RemoveAll(entityDirPath)
